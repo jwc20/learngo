f5ae69c9abe6f1ccb348082d4614ca3e2aa2fcd4
jw <choi.jae100@gmail.com>
2026-02-25 11:44:29 -0800

    add basic

--- projecttut/teatorial/main.go
+++ projecttut/teatorial/main.go
@@ -1,20 +1,50 @@
 package main
 
-func main() {
-	// New Program with inital model and program options
+import (
+	"log"
 
-	// Run the program
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	m := NewModel()
+
+	// New Program with inital model and program options
+	p := tea.NewProgram(m)
+
+	// Run
+	_, err := p.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // Model: app state
+type Model struct {
+	title string
+}
 
 // NewModel: inital model
+func NewModel() Model {
+	return Model{
+		title: "Hello world",
+	}
+}
 
 // Init: kick off the event loop
+func (m Model) Init() tea.Cmd {
+	return nil
+}
 
 // Update: handle Msgs
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
 
 // View: render the string based on the state of our model
+func (m Model) View() string {
+	return m.title
+}
 
 // Cmd
 
