2708081f997f11662a1cb00b76a8b07a25a645c1
jw <choi.jae100@gmail.com>
2026-02-25 11:35:43 -0800

    add outline

---
+++ projecttut/teatorial/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	// New Program with inital model and program options
+
+	// Run the program
+}
+
+// Model: app state
+
+// NewModel: inital model
+
+// Init: kick off the event loop
+
+// Update: handle Msgs
+
+// View: render the string based on the state of our model
+
+// Cmd
+
+// Msg
